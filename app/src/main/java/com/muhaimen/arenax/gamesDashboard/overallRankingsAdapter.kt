3a2b3961873711502c2b7c8df21e25d2fb9b93f4
SyedMustafa20 <samustafa290@gmail.com>
2024-10-09 20:10:05 +0500

    Analytics

--- app/src/main/java/com/muhaimen/arenax/gamesDashboard/overallRankingsAdapter.kt
+++ app/src/main/java/com/muhaimen/arenax/gamesDashboard/overallRankingsAdapter.kt
@@ -7,14 +7,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.GridLabelRenderer
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
+import com.bumptech.glide.Glide
 import com.muhaimen.arenax.R
-import com.muhaimen.arenax.dataClasses.AnalyticsData
 import com.muhaimen.arenax.dataClasses.RankingData
-import com.muhaimen.arenax.gamesDashboard.overallLeaderboard
 
 class overallLeaderboardAdapter(private val rankingsList: List<RankingData>) : RecyclerView.Adapter<overallLeaderboardAdapter.overallLeaderboardViewHolder>() {
 
@@ -26,18 +21,23 @@ class overallLeaderboardAdapter(private val rankingsList: List<RankingData>) : R
         val rank: TextView = itemView.findViewById(R.id.rankNumber)
         val gamerTag: TextView = itemView.findViewById(R.id.gamerTagTextView)
 
-        // Function to populate the graph with data
+        // Function to populate the view with data
         fun bind(data: RankingData) {
             name.text = data.name
             totalHours.text = "Total Hours: ${data.totalHrs}"
-            profilePicture.setImageResource(data.profilePicture)
+
+
+            // Load profile picture using Glide
+            Glide.with(itemView.context)
+                .load(data.profilePicture) // Load the URL
+                .placeholder(R.drawable.circle) // Placeholder image while loading
+                .error(R.drawable.circle)
+                .circleCrop()// Error image if the load fails
+                .into(profilePicture)
+
             rank.text = data.rank.toString()
             gamerTag.text = data.gamerTag
-
-
         }
-
-
     }
 
     // Create new views
@@ -52,30 +52,29 @@ class overallLeaderboardAdapter(private val rankingsList: List<RankingData>) : R
         val rankingsData = rankingsList[position]
         holder.bind(rankingsData)
 
-
-            when (position) {
-                0 -> { // Gold
-                    holder.itemView.setBackgroundColor(Color.parseColor("#FFD700"))
-                    holder.name.setTextColor(Color.WHITE) // Set text color to black
-                    holder.totalHours.setTextColor(Color.WHITE) // Set text color to black
-                    holder.gamerTag.setTextColor(Color.WHITE)
-
-                }
-                1 -> { // Silver
-                    holder.itemView.setBackgroundColor(Color.parseColor("#C0C0C0"))
-                    holder.name.setTextColor(Color.WHITE) // Set text color to black
-                    holder.totalHours.setTextColor(Color.WHITE)
-                    holder.gamerTag.setTextColor(Color.WHITE)
-                }
-                2 -> { // Bronze
-                    holder.itemView.setBackgroundColor(Color.parseColor("#cd7f32"))
-                    holder.name.setTextColor(Color.WHITE) // Set text color to black
-                    holder.totalHours.setTextColor(Color.WHITE)
-                    holder.gamerTag.setTextColor(Color.WHITE)
-                }
-                else -> {
-                    holder.itemView.setBackgroundColor(Color.WHITE) // Default background
-                }
+        // Change background color based on ranking position
+        when (position) {
+            0 -> { // Gold
+                holder.itemView.setBackgroundColor(Color.parseColor("#FFD700"))
+                holder.name.setTextColor(Color.WHITE)
+                holder.totalHours.setTextColor(Color.WHITE)
+                holder.gamerTag.setTextColor(Color.WHITE)
+            }
+            1 -> { // Silver
+                holder.itemView.setBackgroundColor(Color.parseColor("#C0C0C0"))
+                holder.name.setTextColor(Color.WHITE)
+                holder.totalHours.setTextColor(Color.WHITE)
+                holder.gamerTag.setTextColor(Color.WHITE)
+            }
+            2 -> { // Bronze
+                holder.itemView.setBackgroundColor(Color.parseColor("#cd7f32"))
+                holder.name.setTextColor(Color.WHITE)
+                holder.totalHours.setTextColor(Color.WHITE)
+                holder.gamerTag.setTextColor(Color.WHITE)
+            }
+            else -> {
+                holder.itemView.setBackgroundColor(Color.WHITE) // Default background
+            }
         }
     }
 
