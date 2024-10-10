8f712b40d283a44bbac706542a9df5a0421868bc
SyedMuhaimen01 <i210888@nu.edu.pk>
2024-10-11 02:57:07 +0500

    upload Post module

--- app/src/main/java/com/muhaimen/arenax/userProfile/postsAdapter.kt
+++ app/src/main/java/com/muhaimen/arenax/userProfile/postsAdapter.kt
@@ -1,5 +1,6 @@
 package com.muhaimen.arenax.userProfile
 
+import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.gson.Gson
 import com.muhaimen.arenax.R
 import com.muhaimen.arenax.dataClasses.Post
+import com.muhaimen.arenax.uploadContent.ViewPost
+import com.muhaimen.arenax.uploadStory.viewStory
 
 class PostsAdapter(private val postsList: List<Post>) : RecyclerView.Adapter<PostsAdapter.PostViewHolder>() {
 
@@ -20,10 +24,22 @@ class PostsAdapter(private val postsList: List<Post>) : RecyclerView.Adapter<Pos
 
         // Function to bind data to views
         fun bind(post: Post) {
-            val uri = Uri.parse(post.postContent)
 
+            itemView.setOnClickListener {
+
+                val intent = Intent(itemView.context, ViewPost::class.java).apply {
+                    putExtra("MEDIA", post.postContent)
+                    putExtra("Caption", post.caption)
+                    putExtra("Likes", post.likes)
+                    putExtra("Comments", post.comments)
+                    putExtra("Shares", post.shares)
+                    putExtra("trimAudio", post.trimmedAudioUrl)
+                    putExtra("createdAt", post.createdAt)
+                }
+                itemView.context.startActivity(intent) // Start the activity
+            }
             // Check if the media exists before loading
-
+            val uri = Uri.parse(post.postContent)
             Glide.with(itemView.context)
                 .load(uri)
                 .thumbnail(0.1f) // Show a thumbnail while loading
