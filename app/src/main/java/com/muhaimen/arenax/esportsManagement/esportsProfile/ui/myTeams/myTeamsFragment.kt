2bb8d288ed1ecfd826a71b35347f63714d2d62c7
SyedMustafa20 <samustafa290@gmail.com>
2025-03-17 16:48:03 +0500

    Complete Integration of myTeams. Which included fetching teams of which a user is either member or captain. then fetching and displaying details of corresponding teams

--- app/src/main/java/com/muhaimen/arenax/esportsManagement/esportsProfile/ui/myTeams/myTeamsFragment.kt
+++ app/src/main/java/com/muhaimen/arenax/esportsManagement/esportsProfile/ui/myTeams/myTeamsFragment.kt
@@ -1,12 +1,25 @@
 package com.muhaimen.arenax.esportsManagement.esportsProfile.ui.myTeams
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.muhaimen.arenax.R
+import com.muhaimen.arenax.dataClasses.Event
+import com.muhaimen.arenax.dataClasses.Team
+import com.muhaimen.arenax.utils.Constants
+import com.muhaimen.arenax.utils.FirebaseManager
+import org.json.JSONArray
+import org.json.JSONObject
 
 class myTeamsFragment : Fragment() {
 
@@ -15,17 +28,71 @@ class myTeamsFragment : Fragment() {
     }
 
     private val viewModel: MyTeamsViewModel by viewModels()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyTeamsAdapter // Replace with your adapter
+    private var teamsList: MutableList<Team> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_my_teams, container, false)
+        val root = inflater.inflate(R.layout.fragment_my_teams, container, false)
+        recyclerView = root.findViewById(R.id.myTeamsRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = MyTeamsAdapter(teamsList)
+        recyclerView.adapter = adapter
+
+        val firebaseUid = FirebaseManager.getCurrentUserId().toString()
+        fetchTeams(firebaseUid)
+
+        return root
+    }
+
+    // Function to fetch teams using Volley
+    private fun fetchTeams(firebaseUid: String) {
+        val url = "${Constants.SERVER_URL}manageTeams/myTeams/$firebaseUid"
+        val requestQueue = Volley.newRequestQueue(requireContext())
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                try {
+                    val teamsJsonArray: JSONArray = response.getJSONArray("teams")
+                    val teamsList = mutableListOf<Team>()
+
+                    // Parsing the JSON array
+                    for (i in 0 until teamsJsonArray.length()) {
+                        val teamJsonObject: JSONObject = teamsJsonArray.getJSONObject(i)
+                        val team = Team(
+                            teamName = teamJsonObject.getString("team_name"),
+                            gameName = teamJsonObject.getString("game_name"),
+                            teamDetails = "",  // You can modify this based on the API response
+                            teamLocation = "",  // Same here
+                            teamEmail = "",  // Same here
+                            teamCaptain = "",  // Same here
+                            teamTagLine = "",  // Same here
+                            teamAchievements = "",  // Same here
+                            teamLogo = teamJsonObject.getString("team_logo"),
+                            teamMembers = null // Adjust based on response if needed
+                        )
+                        teamsList.add(team)
+                    }
+
+                    // Populating the adapter with the data
+                    adapter.updateData(teamsList)
+
+                } catch (e: Exception) {
+                    Log.e("FetchTeams", "Error parsing response", e)
+                }
+            },
+            { error ->
+                Log.e("FetchTeams", "Error: ${error.message}")
+            }
+        )
+        requestQueue.add(jsonObjectRequest)
     }
 }
---
+++ app/src/main/java/com/muhaimen/arenax/esportsManagement/esportsProfile/ui/myTeams/playersAdapter.kt
@@ -0,0 +1,54 @@
+package com.muhaimen.arenax.esportsManagement.esportsProfile.ui.myTeams
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.muhaimen.arenax.R
+import com.muhaimen.arenax.dataClasses.UserData
+
+class playersAdapter(
+    private var teamMembersData: MutableList<UserData>,
+    ) : RecyclerView.Adapter<playersAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.player_card, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val user = teamMembersData[position]
+            holder.bind(user)
+        }
+
+        override fun getItemCount(): Int = teamMembersData.size
+
+        // This function updates the list of team members when new data is fetched
+        fun updateTeamMembers(newMembersList: List<UserData>) {
+            teamMembersData.clear()
+            teamMembersData.addAll(newMembersList)
+            notifyDataSetChanged() // Refresh the adapter with new data
+        }
+
+        // ViewHolder class to bind the player data to the UI
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val adminNameTextView: TextView = itemView.findViewById(R.id.fullname)
+            private val gamerTagTextView: TextView = itemView.findViewById(R.id.gamerTag)
+            private val profilePicture: ImageView = itemView.findViewById(R.id.profilePicture)
+
+            fun bind(user: UserData) {
+                adminNameTextView.text = user.fullname
+                gamerTagTextView.text = user.gamerTag ?: "N/A"
+                Glide.with(itemView.context)
+                    .load(user.profilePicture ?: R.drawable.battlegrounds_icon_background)
+                    .placeholder(R.drawable.battlegrounds_icon_background)
+                    .error(R.drawable.battlegrounds_icon_background)
+                    .circleCrop()
+                    .into(profilePicture)
+            }
+        }
+    }
