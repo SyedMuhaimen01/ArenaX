b6485c5121725508f5ad7c4f02d64a47cd81caff
SyedMustafa20 <samustafa290@gmail.com>
2025-04-11 18:37:02 +0500

    Final implementation fixed

--- app/src/main/java/com/muhaimen/arenax/esportsManagement/OtherOrganization/otherOrganizationJobsAdapter.kt
+++ app/src/main/java/com/muhaimen/arenax/esportsManagement/OtherOrganization/otherOrganizationJobsAdapter.kt
@@ -18,6 +18,7 @@ class otherOrganizationJobsAdapter(
 
     // Method to update the dataset with new jobs
     fun updateData(newJobWithOrgList: List<JobWithOrganization>) {
+
         notifyDataSetChanged()
     }
 
